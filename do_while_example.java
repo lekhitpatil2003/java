0d79b05058b009bcf837d4a429143f258da40649
Lekhit Patil <patillekhit@gmail.com>
2026-02-13 17:32:36 +0530

    do while example

---
+++ do_while_example.java
@@ -0,0 +1,12 @@
+class do_while_example{
+    public static void main(String[] args) {
+        //syntax of do-while loop
+        //do{
+        //    //code to be executed
+        //}while(condition);
+
+        do{
+            System.out.println("FCT");
+        }while(1>10);
+    }
+}
