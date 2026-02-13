d3ad6c993ee5e7b5bae4ca619486f540afdd29d1
Lekhit Patil <patillekhit@gmail.com>
2026-02-13 18:13:53 +0530

    switch case Example

---
+++ switchExample.java
@@ -0,0 +1,19 @@
+public class switchExample {
+    public static void main(String[] args) {
+        int num = 24;
+        switch(num) {
+            case 1:
+                System.out.println("My NUmber is 1");
+                break;
+            case 24:
+                System.out.println("My NUmber is 24");
+                break;
+            case 3:
+                System.out.println("My NUmber is 3");
+                break;
+            default:
+                System.out.println("No NUmber Matched");
+                break;
+        }
+    }
+}
