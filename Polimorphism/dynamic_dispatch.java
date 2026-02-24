d830a8a84e605b2c44510164ecb38bc9af9f6dad
Lekhit Patil <patillekhit@gmail.com>
2026-02-24 09:12:05 +0530

    dynamic dispatch example

---
+++ Polimorphism/dynamic_dispatch.java
@@ -0,0 +1,43 @@
+package Polimorphism;
+
+//base class or Parent class
+class Phone{
+    public void greet(){
+        System.out.println("Good Morning");
+    }
+
+    public void name(){
+        System.out.println("My name is java from base class...");
+    }
+}
+//Derived Class
+class Smartphone extends Phone{
+    public void swagat(){
+        System.out.println("Shubh Prabhat...");
+    }
+
+    @Override
+    public void name(){
+        System.out.println("My name is java from derived class...");
+    }
+}
+
+public class dynamic_dispatch {
+    public static void main(String[] args) {
+        Phone obj = new Phone();
+        obj.greet();
+        //obj.swagat(); // This will give an error because the reference type is Phone which does not have swagat() method
+        obj.name();
+
+        Smartphone obj2 = new Smartphone();
+        obj2.greet(); // This will work because Smartphone inherits from Phone
+        obj2.swagat(); // This will work because the reference type is Smartphone which has swagat() method
+        obj2.name(); // This will call the name() method of Smartphone class because of method overriding
+
+        Phone obj3 = new Smartphone(); // Upcasting
+        obj3.greet(); // This will work because Phone class has greet() method
+        obj3.name(); // This will call the name() method of Smartphone class because of dynamic dispatch
+        //obj3.swagat(); // This will give an error because the reference type is Phone which does not have swagat() method
+        
+    }
+}
