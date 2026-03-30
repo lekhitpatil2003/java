b8a8753a50c8c490508ca316807c91d3401197cb
Lekhit Patil <149252124+lekhitpatil2003@users.noreply.github.com>
2026-03-30 19:50:28 +0530

    collection framework

---
+++ Collection Framework/ArrayListExample.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        // Creating ArrayList
+        ArrayList<String> names = new ArrayList<>();
+
+        //Adding elements
+        names.add("Lekhit");
+        names.add("Yogesh");
+        names.add("Vaibhav");
+        
+
+        System.out.println(names);
+
+        System.out.println(names.get(1));
+
+        //remove element
+        names.remove("Vaibhav");
+
+        // print all element
+        System.out.println(names);
+
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        System.out.println(num);
+
+        num.remove(Integer.valueOf(2));
+        System.out.println(num);
+    }
+}
---
+++ Collection Framework/ArrayListMethods.java
@@ -0,0 +1,5 @@
+public class ArrayListMethods {
+    public static void main(String[] args) {
+        
+    }
+}
