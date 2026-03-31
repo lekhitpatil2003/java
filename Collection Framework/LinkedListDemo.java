837535a8361503d07a9d50a83c3c84d2b084a474
Lekhit Patil <149252124+lekhitpatil2003@users.noreply.github.com>
2026-03-31 09:34:07 +0530

    linked list

---
+++ Collection Framework/LinkedListDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Java");
+        list.add("c#");
+        list.add("Python");
+        System.out.println(list);
+    }
+}
